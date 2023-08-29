fda754d87b810fd9727bcf167e0bc66f49b1a7c1
Георгий или Виктория <egorkuss@icloud.com>
2023-08-29 22:57:29 +0300

    Я надеюсь передача  данных сработает.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('блять')
+def is_palindrome(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
+
+input_word = input("Введите строку: ")
+is_pal = is_palindrome(input_word)
+print(is_pal)
